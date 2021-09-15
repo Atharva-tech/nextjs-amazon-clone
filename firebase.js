bbca2cad79002e33329ea1147f2f123375faeba7
Atharva-tech <atharvapalkar1@gmail.com>
2021-09-15 18:37:32 +0530

    day 3 complete

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA6weYaxA2JBMUtremcXJigIP-W-walniU",
+  authDomain: "clone-next-936a4.firebaseapp.com",
+  projectId: "clone-next-936a4",
+  storageBucket: "clone-next-936a4.appspot.com",
+  messagingSenderId: "1010845912618",
+  appId: "1:1010845912618:web:16f5c4176a3b90eb7bbd53",
+};
